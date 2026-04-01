86b768a16d6953a7a3fe9d6b6361fd30c6b21804
Darix SAMANI SIEWE <samanidarix@gmail.com>
2026-04-01 15:19:29 +0100

    use simple fetch instaed of axios

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,16 +1,17 @@
-import axios from "axios";
 export {};
 
-const API = "http://0.0.0.0:8080/api"
+const API = "http://0.0.0.0:8080/api";
 
-localStorage.setItem("apiUrl", API)
+if (!localStorage.getItem("apiUrl")) {
+    localStorage.setItem("apiUrl", API);
+}
 
-export const axionsInstance = axios.create({
-    baseURL: localStorage.getItem("apiUrl") || API,
-    maxRedirects: 5, // Number of redirects to follow automatically
-    headers: {
-        "Accept": "*",
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "Origin",
-    },
-})
+export const API_BASE_URL = localStorage.getItem("apiUrl") || API;
+
+export const buildApiUrl = (path: string) => {
+    if (!path) {
+        return API_BASE_URL;
+    }
+
+    return `${API_BASE_URL}${path.startsWith("/") ? path : `/${path}`}`;
+};
